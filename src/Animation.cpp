6c0a3f035811f0f340c89a8c2b8ac0770bc356f6
AidCheng <cn.aiden.cheng@gmail.com>
2024-10-07 16:02:07 +0100

    remove_test_doc

--- src/Animation.cpp
+++ src/Animation.cpp
@@ -20,3 +20,28 @@ Animation::Animation(std::string name, const sf::Texture& t, size_t frameCount, 
   m_sprite.setOrigin(m_size.x/2.0f, m_size.y/2.0f);
   m_sprite.setTextureRect(sf::IntRect(std::floor(m_currentFrame)*m_size.x, 0, m_size.x, m_size.y));
 }
+
+void Animation::update()
+{
+
+}
+
+bool Animation::hasEnded() const
+{
+
+}
+
+const std::string& Animation::getName() const
+{
+
+}
+
+const Vec2& Animation::getSize() const
+{
+
+}
+
+sf::Sprite& Animation::getSprite() const
+{
+
+}
